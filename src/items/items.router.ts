2ed64248370c4f7ac5cd0a2232d51684f607318f
David Soards <david@soards.me>
2022-10-24 01:02:40 -0400

    add Auth0

--- src/items/items.router.ts
+++ src/items/items.router.ts
@@ -5,6 +5,7 @@
 import express, { Request, Response } from 'express';
 import * as ItemService from './items.service';
 import { BaseItem, Item } from './item.interface';
+import { checkJwt } from '../middleware/authz.middleware';
 
 /**
  * Router Definition
@@ -39,6 +40,9 @@ itemsRouter.get('/:id', async (req: Request, res: Response) => {
     res.status(500).send(err.message);
   }
 });
+
+// Mount authorization middleware
+itemsRouter.use(checkJwt);
 
 // POST items
 itemsRouter.post('/', async (req: Request, res: Response) => {
